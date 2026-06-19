d6046b8c20ce1939c3e33d43820603f16e748132
rkimonvai-cmd <rkimonvai@gmail.com>
2026-06-20 05:06:24 +0600

    Update thread.js

--- Script/commands/thread.js
+++ Script/commands/thread.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "thread",  
 	version: "2.0.0",  
 	hasPermssion: 2,  
-	credits: "SHAHADAT SAHU",  
+	credits: "EMON BOSS",  
 	description: "Ban or unban a group",  
 	commandCategory: "system",  
 	usages: "[unban/ban/search] [ID or text]",  
