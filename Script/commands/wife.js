50bb011b7fba488b28386f761bc26a748c8a54d4
rkimonvai-cmd <rkimonvai@gmail.com>
2026-06-20 04:52:52 +0600

    Update wife.js

--- Script/commands/wife.js
+++ Script/commands/wife.js
@@ -6,7 +6,7 @@ module.exports.config = {
   name: "wife",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "SHAHADAT SAHU",
+  credits: "EMON BOSS",
   description: "Generate a couple banner image using sender and target Facebook UID via Avatar Canvas API",
   commandCategory: "banner",
   usePrefix: true,
