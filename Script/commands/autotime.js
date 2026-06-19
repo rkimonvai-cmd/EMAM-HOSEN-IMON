9309d6e45850d8eebd053495bd5bddd583141af4
rkimonvai-cmd <rkimonvai@gmail.com>
2026-06-20 05:20:27 +0600

    Update autotime.js

--- Script/commands/autotime.js
+++ Script/commands/autotime.js
@@ -6,7 +6,7 @@ module.exports.config = {
     name: 'autosent',
     version: '10.0.1',
     hasPermssion: 0,
-    credits: 'Shahadat Islam',
+    credits: 'EMON BOSS',
     description: 'Automatically sends messages at scheduled times (BD Time)',
     commandCategory: 'group messenger',
     usages: '[]',
