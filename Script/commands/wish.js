9893d9f3e134b289b1cbb6b60a43e61678b00d38
rkimonvai-cmd <rkimonvai@gmail.com>
2026-06-20 04:52:03 +0600

    Update wish.js

--- Script/commands/wish.js
+++ Script/commands/wish.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "wish",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "SHAHADAT SAHU",
+ credits: "EMON BOSS",
  description: "Happy birthday wish for your friends",
  commandCategory: "Birthday",
  usages: "@tag",
